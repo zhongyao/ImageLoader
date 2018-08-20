862d6373c845cea046aab02f413dc04a80aecec6
zhongyao <zhongyao@alibaba-inc.com>
2018-08-20 17:05:10 +0800

    ImageLoader

---
+++ app/src/main/java/com/hongri/adapter/RecyclerAdapter.java
@@ -0,0 +1,60 @@
+package com.hongri.adapter;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import com.hongri.loader.ImageLoader;
+import com.hongri.view.R;
+
+/**
+ * @author zhongyao
+ * @date 2018/8/20
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+    private Context mContext;
+    private LayoutInflater inflater;
+    private ArrayList<String> imageUrls;
+    private ImageLoader mImageLoader;
+    private static final int IMAGE_SIZE = 200;
+
+    public RecyclerAdapter(Context context, ArrayList<String> imageUrls, ImageLoader mImageLoader) {
+        mContext = context;
+        inflater = LayoutInflater.from(context);
+        this.imageUrls = imageUrls;
+        this.mImageLoader = mImageLoader;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.view_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        mImageLoader.bindBitmap(imageUrls.get(position), holder.mIv, IMAGE_SIZE, IMAGE_SIZE);
+
+        //holder.mIv.setImageBitmap(mImageLoader.loadBitmap(imageUrls.get(position), IMAGE_SIZE, IMAGE_SIZE));
+    }
+
+    @Override
+    public int getItemCount() {
+        return (imageUrls != null && imageUrls.size() > 0) ? imageUrls.size() : 0;
+    }
+
+    public class MyViewHolder extends ViewHolder {
+        private ImageView mIv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            mIv = (ImageView)itemView.findViewById(R.id.item);
+        }
+    }
+}
--- app/src/main/java/com/hongri/view/MainActivity.java
+++ app/src/main/java/com/hongri/view/MainActivity.java
@@ -1,80 +1,31 @@
 package com.hongri.view;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.ContentResolver;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.provider.MediaStore;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
-import android.widget.Toast;
+import com.hongri.adapter.RecyclerAdapter;
 import com.hongri.bitmap.BitmapFactoryUtil;
+import com.hongri.loader.ImageLoader;
 import com.hongri.utils.ImageUtil;
 
 /**
  * @author hongri
  */
 public class MainActivity extends Activity implements View.OnClickListener {
-    private ProgressDialog mProgressDialog;
-    /**
-     * 存储文件夹中的图片数量
-     */
-    private int mPicsSize;
-    /**
-     * 图片数量最多的文件夹
-     */
-    private File mImgDir;
-    /**
-     * 所有的图片
-     */
-    private List<String> mImgs;
 
     private Button btnBmp;
     public static ImageView iv;
-
-    private GridView mGirdView;
-    private ListAdapter mAdapter;
-    /**
-     * 临时的辅助类，用于防止同一个文件夹的多次扫描
-     */
-    private HashSet<String> mDirPaths = new HashSet<String>();
-
-    private Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(android.os.Message msg) {
-            mProgressDialog.dismiss();
-            mImgs = Arrays.asList(mImgDir.list(new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String filename) {
-                    if (filename.endsWith(".jpg")) {
-                        return true;
-                    }
-                    return false;
-                }
-            }));
-            /**
-             * 可以看到文件夹的路径和图片的路径分开保存，极大的减少了内存的消耗；
-             */
-            mAdapter = new MyAdapter(getApplicationContext(), mImgs,
-                mImgDir.getAbsolutePath());
-            mGirdView.setAdapter(mAdapter);
-        }
-
-        ;
-    };
+    private RecyclerView rv;
+    private RecyclerAdapter mAdapter;
+    private ArrayList<String> mImageUrls;
+    private ImageLoader mImageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,78 +34,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         btnBmp = (Button)findViewById(R.id.btnBmp);
         iv = (ImageView)findViewById(R.id.iv);
-        mGirdView = (GridView)findViewById(R.id.id_gridView);
+        rv = (RecyclerView)findViewById(R.id.rv);
+
+        mImageUrls = ImageUtil.getImageUrls();
+        mImageLoader = ImageLoader.build(this);
+
+        mAdapter = new RecyclerAdapter(this, mImageUrls, mImageLoader);
+        rv.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
+        rv.setLayoutManager(new GridLayoutManager(MainActivity.this, 3));
+        rv.setAdapter(mAdapter);
 
         btnBmp.setOnClickListener(this);
-        //getImages();
-
-    }
-
-    /**
-     * 利用ContentProvider扫描手机中的图片，此方法在运行在子线程中 完成图片的扫描，最终获得jpg最多的那个文件夹
-     */
-    private void getImages() {
-        if (!Environment.getExternalStorageState().equals(
-            Environment.MEDIA_MOUNTED)) {
-            Toast.makeText(this, "暂无外部存储", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        // 显示进度条
-        mProgressDialog = ProgressDialog.show(this, null, "正在加载...");
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                Uri mImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                ContentResolver mContentResolver = MainActivity.this
-                    .getContentResolver();
-
-                // 只查询jpeg和png的图片
-                Cursor mCursor = mContentResolver.query(mImageUri, null,
-                    MediaStore.Images.Media.MIME_TYPE + "=? or "
-                        + MediaStore.Images.Media.MIME_TYPE + "=?",
-                    new String[] {"image/jpeg", "image/png"},
-                    MediaStore.Images.Media.DATE_MODIFIED);
-
-                while (mCursor.moveToNext()) {
-                    // 获取图片的路径
-                    String path = mCursor.getString(mCursor
-                        .getColumnIndex(MediaStore.Images.Media.DATA));
-                    // 获取该图片的父路径名
-                    File parentFile = new File(path).getParentFile();
-                    String dirPath = parentFile.getAbsolutePath();
-
-                    //利用一个HashSet防止多次扫描同一个文件夹（不加这个判断，图片多起来还是相当恐怖的~~）
-                    if (mDirPaths.contains(dirPath)) {
-                        continue;
-                    } else {
-                        mDirPaths.add(dirPath);
-                    }
-
-                    int picSize = parentFile.list(new FilenameFilter() {
-                        @Override
-                        public boolean accept(File dir, String filename) {
-                            if (filename.endsWith(".jpg")) {
-                                return true;
-                            }
-                            return false;
-                        }
-                    }).length;
-                    if (picSize > mPicsSize) {
-                        mPicsSize = picSize;
-                        mImgDir = parentFile;
-                    }
-                }
-                mCursor.close();
-                //扫描完成，辅助的HashSet也就可以释放内存了
-                mDirPaths = null;
-                // 通知Handler扫描图片完成
-                mHandler.sendEmptyMessage(0x110);
-
-            }
-        }).start();
-
     }
 
     @Override
